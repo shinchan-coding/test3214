7ddc535aca094f508b65a1bd0e5411fd4504da22
shinchan-coding <1609kittujaiswal@gmail.com>
2024-11-18 20:42:32 +0530

    Create assignment1.cpp

---
+++ assignment1.cpp
@@ -0,0 +1,38 @@
+//question 1
+#include<iostream>
+using namespace std;
+ int main(){
+    cout << "PHYSICS"<<endl;
+    cout << "WALLAH"<<endl;
+ 
+ //question 2
+ cout << 5 + 5 <<endl;
+ cout << 5 * 2 <<endl;
+ //question 3
+ //int takes 4 byte
+ //bool takes 1 byte
+ // float takes 4 byte
+ // question 4
+//  void main(){
+// 	int a=4;
+// int b=5;
+// a++;b--;
+// cout<<++a<<” “<<b–-;
+// }
+// it will print 6 3
+// question 5
+float r = 10;
+float circumferenceofcircle = 2*3.14*r;
+cout << "circumference of circle is " << circumferenceofcircle;
+// 01PWskills
+
+//  _FLOAT
+
+//  int
+
+//  FLOAT
+
+//  You will succeed 
+//FLOAT WILL BE A VARIABLE NAME
+
+ }
