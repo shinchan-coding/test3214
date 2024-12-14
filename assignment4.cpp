060cdfcbb5bf9a282a9cc6495a03a6f0bbbfdbf7
shinchan-coding <1609kittujaiswal@gmail.com>
2024-12-14 21:51:40 +0530

    Add files via upload

---
+++ assignment4.cpp
@@ -0,0 +1,108 @@
+// Write a program to count the minimum number of notes in a given 
+// amount using the switch statement.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int amount;
+//     cout <<"Enter the amount : ";
+//     cin >> amount;
+//     int n1,n2,n5,n10,n20,n50,n100,n500;
+// 	n1 = n2 = n5 = n10 = n20 = n50 = n100 = n500 =0;
+//     switch(amount>=500)
+// 	{
+// 		case 1:
+// 			n500 = amount/500;
+//         	amount -= n500 * 500;
+//         break;
+// 	}
+// 	switch(amount >=100)
+// 	{
+// 		case 1:
+// 			n100 = amount/100;
+//         	amount -= n100 * 100;
+//         break;
+// 	}
+// 	switch(amount >=50)
+// 	{
+// 		case 1:
+// 			n50 = amount/50;
+//         	amount -= n50 * 50;
+// 	}
+// 	switch(amount >=20)
+// 	{
+// 		case 1:
+// 			n20 = amount/20;
+//         	amount -= n20 * 20;
+//         break;
+// 	}
+// 	switch(amount >=10)
+// 	{
+// 		case 1:
+// 			n10 = amount/10;
+//         	amount -= n10 * 10;
+//         break;
+// 	}
+// 	cout << "500 = " << n500 <<endl;
+//     cout << "100 = " << n100 <<endl;
+//     cout << "50 = " << n50 <<endl;
+//     cout << "20 = " << n20 <<endl;
+//     cout << "10 = " << n10 <<endl;
+// }
+// Predict the output:
+// #include<iostream>
+// using namespace std;
+// int main( ) {
+// int a = 5, b, c ;
+// b = a = 15 ;
+// c = a < 15 ;
+// cout << "a = " << a << ", b = " << b << " , c = "<< c ;
+// return 0;
+// }
+// a = 15, b = 15 , c = 0//output
+// Predict the output:
+//#include<iostream>
+// using namespace std;
+// int main() {
+// int x = 3 ;
+// float y = 3.0 ;
+// if (x == y)
+// cout <<"x and y are equal" ;
+// else
+// cout << "x and y are not equal" ;
+// return 0;
+// }
+// x and y are equal//output
+// predict the output:
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int test = 0;
+// cout << "First character " << '1' << endl;
+// cout << "Second character " << (test ? 3 : '1') << endl;
+// return 0;
+// }
+// First character 1
+// Second character 49(ascii value of 1)
+// predict the output:
+// #include <iostream>
+// using namespace std;
+// int main(){
+// int a = 18; int b = 12;
+// bool t = (a > 20 && b < 15)? true : false;
+// cout <<"Value of t: " << t ;
+// return 0;
+// }
+// false//output
+// predict the output:
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+// int number = -4;
+// char result;
+// result = number > 0 ? 'P' : 'N';
+// cout << result << endl;
+// return 0;
+// }
+// N //output
